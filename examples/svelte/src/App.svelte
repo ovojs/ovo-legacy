953dac6aa3952511053a38cd9600ba8e6a9a7f34
mivinci <1366723936@qq.com>
2021-07-19 18:30:13 +0800

    fix a typo

--- examples/svelte/src/App.svelte
+++ examples/svelte/src/App.svelte
@@ -29,7 +29,7 @@
   <h2>A Svelte Web Component for Comments</h2>
   <p>
     Inspired by <a href="https://github.com/DIYgod/OwO">OωO</a>. Available on
-    <a href="https://github.com/Mivinci/OvO">GitHub</a>. Docs in
+    <a href="https://github.com/ovojs/OvO">GitHub</a>. Docs in
     <a href="https://github.com/ovojs/OvO/blob/main/README.md">README</a>
   </p>
   <div>
