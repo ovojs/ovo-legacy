1a6e2b6d56a1ae2325609269b16531bda3afba9c
mivinci <1366723936@qq.com>
2021-08-04 11:00:34 +0800

    fix site

--- examples/svelte-ts/src/locales.ts
+++ examples/svelte-ts/src/locales.ts
@@ -2,6 +2,6 @@ import en from "@ovojs/ovo/lib/locales/en.json";
 import zh_Hans from "@ovojs/ovo/lib/locales/zh-Hans.json";
 
 export default {
-  "简体中文": zh_Hans,
   "English": en,
+  "简体中文": zh_Hans,
 }
